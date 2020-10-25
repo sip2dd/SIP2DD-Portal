3ea8c07a70cd17109fc06e0432aec37311d63c90
Reza Gunawan <rezagunawan999@gmail.com>
2020-10-25 21:41:12 +0700

    perubahan url

--- app/Http/Traits/ApiContentsTrait.php
+++ app/Http/Traits/ApiContentsTrait.php
@@ -11,31 +11,32 @@ trait ApiContentsTrait{
     public function getApiContents(String $url = ""){
         $client = new Client();
         $url = config('url.base_api') . $url;
+        //$url = "http://103.18.117.44/sicantik-ws/api/TemplateData/keluaran/1527.json";
+        $request = $client->get($url);
+        //$request = $client->request('GET', $url,['timeout' => 5, 'connect_timeout' => 5]);
+        $response = $request->getBody()->getContents();
+        $data = json_decode($response, true);
+        
+        try{
+            $url = config('url.base_api') . $url;
             //$url = "http://103.18.117.44/sicantik-ws/api/TemplateData/keluaran/1527.json";
-            //$request = $client->get($url);
-            $request = $client->request('GET', $url,['timeout' => 5, 'connect_timeout' => 5]);
+            $request = $client->get($url);
+            //$request = $client->request('GET', $url,['timeout' => 5, 'connect_timeout' => 5]);
             $response = $request->getBody()->getContents();
-            $data = json_decode($response, true);
-        // try{
-        //     $url = config('url.base_api') . $url;
-        //     //$url = "http://103.18.117.44/sicantik-ws/api/TemplateData/keluaran/1527.json";
-        //     $request = $client->get($url);
-        //     //$request = $client->request('GET', $url,['timeout' => 5, 'connect_timeout' => 5]);
-        //     $response = $request->getBody()->getContents();
-        //     $data = json_decode($response, true);    
-        // }catch (ConnectException $e) {
-        //     //$data = "connect";
-        //     $data = null;
-        // }catch (RequestException $e){
-        //     //$data = "request";
-        //     $data = null;
-        // }catch (TransferException $e){
-        //     //$data = "transfer";
-        //     $data = null;
-        // }catch(GuzzleHttp\Exception\GuzzleException $e) {
-        //     //$data = "guzzle";
-        //     $data = null;
-        // }
+            $data = json_decode($response, true);    
+        }catch (ConnectException $e) {
+            //$data = "connect";
+            $data = null;
+        }catch (RequestException $e){
+            //$data = "request";
+            $data = null;
+        }catch (TransferException $e){
+            //$data = "transfer";
+            $data = null;
+        }catch(GuzzleHttp\Exception\GuzzleException $e) {
+            //$data = "guzzle";
+            $data = null;
+        }
 
         return $data;
     }
